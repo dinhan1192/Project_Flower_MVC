486df6f4729816c69aa9bc5141d4a641e1c66d97
Thao <thaonttth1807035@fpr.edu.vn>
2020-03-16 16:38:16 +0700

    Detail Flower Customer.

--- Project_MVC/Controllers/FlowersController.cs
+++ Project_MVC/Controllers/FlowersController.cs
@@ -252,6 +252,7 @@ namespace Project_MVC.Controllers
             {
                 return HttpNotFound();
             }
+            ViewBag.Flowers = mySQLFlowerService.GetList();
             return View(flower);
         }
 
