93d51afe1c829952c4ce5723678ea373a1663de3
dinhan1192 <dinhan1192@gmail.com>
2020-03-18 15:37:38 +0700

    Rating and comment

---
+++ Project_MVC/Services/RatingFlowerService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using Project_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Project_MVC.Services
+{
+    public class RatingFlowerService : IRatingFlowerService
+    {
+        private MyDbContext _db;
+        public MyDbContext DbContext
+        {
+            get { return _db ?? HttpContext.Current.GetOwinContext().Get<MyDbContext>(); }
+            set { _db = value; }
+        }
+
+        public void CreateRating(decimal rating, string flowerCode, string userId)
+        {
+            var item = new RatingFlower()
+            {
+                FlowerCode = flowerCode,
+                UserId = userId,
+                Rating = rating
+            };
+
+            DbContext.RatingFlowers.Add(item);
+            DbContext.SaveChanges();
+        }
+
+        public string UpdateFlowerRating(decimal rating, string flowerCode, string type)
+        {
+            var existFlower = DbContext.Flowers.Find(flowerCode);
+            var listRatingFlower = DbContext.RatingFlowers.Where(s => s.FlowerCode == flowerCode).ToList();
+            var countRatingFlower = listRatingFlower.Count;
+            if (countRatingFlower != 0 && listRatingFlower != null)
+            {
+                if (existFlower.Rating == null)
+                {
+                    existFlower.Rating = 0;
+                }
+
+                switch (type)
+                {
+                    case Constant.CreateRating:
+                        existFlower.Rating = (existFlower.Rating + rating) / countRatingFlower;
+                        break;
+                    case Constant.UpdateRating:
+                        existFlower.Rating = listRatingFlower.Select(s => s.Rating).Sum() / countRatingFlower;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            DbContext.Flowers.AddOrUpdate(existFlower);
+            DbContext.SaveChanges();
+
+            return DbContext.Flowers.Find(flowerCode).Code;
+        }
+
+        public void UpdateRating(decimal rating, int? ratingFlowerId)
+        {
+            var existRatingFlower = DbContext.RatingFlowers.Find(ratingFlowerId);
+            existRatingFlower.Rating = rating;
+            DbContext.RatingFlowers.AddOrUpdate(existRatingFlower);
+            DbContext.SaveChanges();
+        }
+    }
+}
